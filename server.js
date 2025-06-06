8fa872505f2c6b898d47cff557c48cb99a7af3e1
PratikDevelops <kepratik8@gmail.com>
2025-06-06 20:13:39 +0530

    backend code added

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import "dotenv/config"
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+
+
+const app = express()
+
+const port = process.env.PORT || 4000;
+connectDB()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true}))
+
+
+
+app.get("/",(req,res)=>res.send("API WORKING"))
+
+app.use("/api/auth",authRouter)
+
+
+
+
+
+
+app.listen(port,()=> console.log(`server stared on PORT : ${port}`))
